eeab637a076f85c7410133f1dbb2a8b1a2257ca3
janksand <sanyika717022@gmail.com>
2019-10-08 20:19:23 +0200

    add view implenetations

--- src/view/View.java
+++ src/view/View.java
@@ -5,37 +5,67 @@
  */
 package view;
 
+import domain.Bet;
+import domain.Currency;
+import domain.Outcome;
 import domain.OutcomeOdd;
 import domain.Player;
 import domain.SportEvent;
 import domain.Wager;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
  * @author Sanyi
  */
-public class View implements IView{
+public class View implements IView {
 
     @Override
     public Player readPlayerData() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Player player = new Player();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is you name ?");
+        player.setName(scanner.nextLine());
+
+        System.out.println("How much monez do you have(more than 0) ?");
+        player.setBalance(new BigDecimal(scanner.nextLine()));
+
+        System.out.println("What is you currency ?");
+        player.setCurrency(Currency.valueOf(scanner.nextLine()));
+
+        return player;
     }
 
+    ;
+    
     @Override
     public void printWelcomeMessage(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Welcome " + player.getName() + "!");
     }
 
     @Override
     public void printBalance(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println(player.getBalance());
     }
 
     @Override
     public void printOutcomeOdds(List<SportEvent> sportEventList) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int index = 0;
+        for (SportEvent sportEvent : sportEventList) {
+            for (Bet bet : sportEvent.getBets()) {
+                for (Outcome outcome : bet.getWinnerOutcome()) {
+                    for (OutcomeOdd outcomeOdd : outcome.getOutcomeOdds()) {
+                        index++;
+                        System.out.println(index + "Sport event: " + sportEvent.getTitle()
+                                + "(start: " + sportEvent.getStartDate() + "), Bet " + bet.getDescription() + ", Outcome: " + outcome.getBet().getDescription()
+                                + ", Actual odd: " + outcomeOdd.getValue() + ", Valid between" + outcomeOdd.getValidFrom() + "and" + outcomeOdd.getValidUntil() + ".");
+                    }
+                }
+            }
+        }
     }
 
     @Override
@@ -50,12 +80,12 @@ public class View implements IView{
 
     @Override
     public void printNotEnoughBalance(Player player) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("You don't have enough money, your balance is " + player.getBalance() + " " + player.getCurrency());
     }
 
     @Override
     public void printResults(Player player, List<Wager> wagerList) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
