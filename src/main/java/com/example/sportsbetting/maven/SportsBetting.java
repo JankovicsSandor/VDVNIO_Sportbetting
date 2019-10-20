3d74e1ec3c6e688c5a39d865f7978f890d800bb1
janksand <sanyika717022@gmail.com>
2019-10-20 10:01:18 +0200

    Add logging

--- src/main/java/com/example/sportsbetting/maven/SportsBetting.java
+++ src/main/java/com/example/sportsbetting/maven/SportsBetting.java
@@ -4,9 +4,13 @@
  * and open the template in the editor.
  */
 package com.example.sportsbetting.maven;
+
 import com.example.sportsbetting.app.App;
 import com.epam.training.sportsbetting.config.AppConfig;
 import com.example.sportsbetting.language.LanguageService;
+import java.text.MessageFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -19,17 +23,20 @@ public class SportsBetting {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        String language="";
+        Logger logger = LoggerFactory.getLogger(SportsBetting.class);
+
+        String language;
         String country;
-        if(args.length !=2){
-            language="en";
+        if (args.length != 2) {
+            language = "en";
             country = "US";
-        }else{
-            language=args[0];
-            country=args[1];
-        }        
-        LanguageService.setLocale(language, country);      
+        } else {
+            language = args[0];
+            country = args[1];
+        }
+        MessageFormat formatter = new MessageFormat("Set locatization to {0} {1}");
+        logger.info(formatter.format(new Object[]{language, country}));
+        LanguageService.setLocale(language, country);
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class)) {
             App application = context.getBean((App.class));
             application.play();
